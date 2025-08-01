2bb7ffa1f53803360376bae29f4927381bd3f936
Athene <athene.mutyambizi@wundermanthompson.com>
2025-08-01 07:14:04 +0200

    fix: Update for time changes.

--- Productivity101/src/app.js
+++ Productivity101/src/app.js
@@ -10,6 +10,31 @@ const sslOptions = {
   key: process.env.SSL_KEY_FILE ? fs.readFileSync(process.env.SSL_KEY_FILE) : undefined,
   cert: process.env.SSL_CRT_FILE ? fs.readFileSync(process.env.SSL_CRT_FILE) : undefined,
 };
+let current_date = new Date();
+let set_date = sessionStorage.getItem("current_date");
+let seconds_worked = sessionStorage.getItem("hours_worked");
+let working_period = 20 * 60;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+if (`${current_date}` != `${set_date}`) {
+   // start a new session
+   sessionStorage.setItem("seconds_worked", 0);
+}
+else {
+   setInterval(() => {
+     // Add counter to time.
+     seconds_worked = sessionStorage.getItem("seconds_worked");
+     sessionStorage.setItem("seconds_worked", 0);
+    }, 1000);
+}
+
+if (seconds_worked % working_period == 0) {
+  alert('Time for a break');
+  await sleep(300000);
+}
+
 
 app.use("/static", express.static(path.join(__dirname, "static")));
 
